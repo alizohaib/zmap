c532b546c971b0d4f175478ead121d3399dcf425
Ali Zohaib <alizohaib1995@gmail.com>
2024-02-11 23:43:07 -0500

    asdasd

--- src/probe_modules/module_forbidden6_scan.c
+++ src/probe_modules/module_forbidden6_scan.c
@@ -134,7 +134,7 @@ static int forbidden6scan_make_packet2(void *buf, UNUSED size_t *buf_len,
 	tcp_header->th_sport =
 	    htons(get_src_port(num_ports, probe_num, validation));
 	tcp_header->th_seq = validation[0];
-	tcp_header->th_ack = 0;
+	tcp_header->th_ack = 123;
 	tcp_header->th_sum = 0;
 
 	tcp_header->th_sum = ipv6_payload_checksum(
